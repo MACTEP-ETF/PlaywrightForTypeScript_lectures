9594b158624f6addc383926f9433fd8f33b0074a
“Vladimirs <vladimirs.fedorovics@testdevlab.com>
2026-03-03 18:34:55 +0200

    .

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,15 +1,13 @@
-import test from "@playwright/test"
+import test, { expect } from "@playwright/test";
 
 test("Login test", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/")
-  
-  // Login
+    // Navigate to SauceDemo
+    await page.goto("https://www.saucedemo.com/")
+    await expect(page).toHaveTitle("Swag Labs")
 
-  await page.fill("#user-name", "standard_user")
-  await page.fill("#password", "secret_sauce")
-  await page.click("[data-test='login-button']")
-
-  await page.getByTestId('login-button').click()
-
-  await page.waitForTimeout(2000)
+    // Login
+    await page.fill("#user-name", "standard_user")
+    await page.fill("#password", "secret_sauce")
+    await page.getByTestId('login-button').click() // await page.click("[data-test='login-button']")
+    await expect(page.getByTestId('title')).toHaveText('Products')
 });
